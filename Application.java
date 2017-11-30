fdc7831f4701e6d4ad6636e4220cbde1ae90f7b6
milunkagrin <34059542+milunkagrin@users.noreply.github.com>
2017-11-30 17:41:40 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -4,8 +4,8 @@ public class Application {
     public static void main(String[] args) {
          MessageStore zin = new MessageStore();
 
-         User user = new User("Вася", "Иванов", "mail");
-         Message m1 = new Message(1, "hello", user );
+         User user = new User("Вася", "Иванов", "mail");           // не уверена, что я правильно поняла, как это все должно работать, но 
+         Message m1 = new Message(1, "hello", user );              // сообщения записываются в массив.
          Message m2 = new Message(2, "hello", user );
          Message m3 = new Message(3, "hello", user );
          Message m4 = new Message(4, "hello", user );
